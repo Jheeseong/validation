1514de18560f4a20bc7d2bb691c2bb7a0a57105a
Jheeseong <gmltjd6801@gmail.com>
2022-03-21 23:31:37 +0900

    Initial commit

---
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,33 @@
+package hello.itemservice.domain.item;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class Item {
+    //@NotNull(groups = UpdateCheck.class)
+    private Long id;
+    //@NotBlank(groups = {CreateCheck.class, UpdateCheck.class})
+    private String itemName;
+
+    //@NotNull(groups = {CreateCheck.class, UpdateCheck.class})
+    //@Range(min = 1000, max = 1000000, groups = {CreateCheck.class, UpdateCheck.class})
+    private Integer price;
+
+    //@NotNull(groups = {CreateCheck.class, UpdateCheck.class})
+    //@Max(value = 9999, groups = CreateCheck.class)
+    private Integer quantity;
+
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
